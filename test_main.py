69d418a0122717aa83d0050815294280e34dc1c2
Mai Linh <mailinh@hello-charles.com>
2022-05-10 02:01:17 +0200

    test: :white_check_mark: add test cases

--- test_main.py
+++ test_main.py
@@ -1,3 +1,4 @@
+from urllib import response
 from fastapi.testclient import TestClient
 
 from main import app
@@ -14,6 +15,26 @@ def test_calculate_score():
         "/scoring",
         files={
             "file": ("filename", open("input.txt", "rb"), "text/plain"),
+        }
+    )
+    assert response.status_code == 200
+    assert response.json() == {"A": 1.75, "B": 1.5, "C": 1.0}
+
+def test_calculate_score_with_empty_input():
+    response = test_client.post(
+        "/scoring",
+        files={
+            "file": ("filename", open("empty_input.txt", "rb"), "text/plain"),
+        }
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Input file is empty"}
+
+def test_calculate_score_only_first_invitation_counts():
+    response = test_client.post(
+        "/scoring",
+        files={
+            "file": ("filename", open("only_first_invite_input.txt", "rb"), "text/plain"),
         }
     )
     assert response.status_code == 200
